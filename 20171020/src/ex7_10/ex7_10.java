c4af891915f71842924e2cb430af81c27f4f0914
이수정 <@DESKTOP-VNPBI7F>
2017-10-25 20:42:26 +0900

    20171025

---
+++ 20171020/src/ex7_10/ex7_10.java
@@ -0,0 +1,42 @@
+package ex7_10;
+
+class MyTv2{
+	boolean isPowerOn;
+	int channel;
+	int volume;
+	
+	final int MAX_VOLUME = 100;
+	final int MIN_VOLUME = 0;
+	final int MAX_CHANNEL = 100;
+	final int MIN_CHANNEL = 1;
+	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	public int getVolume() {
+		return volume;
+	}
+	
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
+	public int getChannel() {
+		return channel;
+	}
+}
+
+public class ex7_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyTv2 t = new MyTv2();
+		
+		t.setChannel(10);
+		System.out.println("CH : "+t.getChannel());
+		t.setVolume(20);
+		System.out.println("VOL : "+t.getVolume());
+	}
+
+}
